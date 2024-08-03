b71fd55b199a41fb78ff8ae602b87ebbf968f95b
antoineleno <johndoe@example.com>
2024-08-04 02:42:05 +0800

    AirBnB

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,7 @@
 from models.state import State
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify, abort, request, make_response
+from flask import jsonify, abort, request
 
 
 @app_views.route("/states", methods=["GET"])
@@ -50,17 +50,25 @@ def post_a_state():
     state.save()
     return jsonify(state.to_dict()), 200
 
-@app_views.route('/states/<string:state_id>', methods=['PUT'],
-                 strict_slashes=False)
-def put_state(state_id):
-    """update a state"""
-    state = storage.get("State", state_id)
+
+@app_views.route("/states/<state_id>", methods=["PUT"])
+def update_single_state(state_id):
+    """Update a State object by id"""
+    if not request.is_json:
+        abort(400, description="Not a JSON")
+
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    for attr, val in request.get_json().items():
-        if attr not in ['id', 'created_at', 'updated_at']:
-            setattr(state, attr, val)
-    state.save()
-    return jsonify(state.to_dict())
+
+    arguments = request.get_json()
+
+    if "name" not in arguments:
+        abort(400, description="Missing name")
+
+    for key, value in arguments.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(state, key, value)
+
+    storage.save()
+    return jsonify(state.to_dict()), 200
