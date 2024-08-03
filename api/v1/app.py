5ea99038ddb9001a72b57bb72b0d80e561c8711e
Amadou001 <bamadou634@gmail.com>
2024-08-03 18:39:58 +0800

    AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ API module
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask import jsonify
 import os
 
 
@@ -18,6 +19,13 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """Close the storage"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found_error(error):
+    """Return a JSON response for 404 errors."""
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
+    return response
 
 
 if __name__ == "__main__":
